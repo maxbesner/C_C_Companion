ed49639287e5349e9d5a03af356028fa79232b7f
telepiggy <Rocklobster1!>
2020-09-13 15:24:34 -0400

    Continued working on image functionality

--- pages/cards/cards.php
+++ pages/cards/cards.php
@@ -10,250 +10,63 @@ include_once('SummonAction.php');
 include_once('header.php');
 include_once('./templates/ActionTemplate.php');
 
-function loadCardsFromFile()
-{
-    $cards = array();
+function createNumericallyIndexedArray($array){
+    $numArray = array();
 
-    $f = "C:/xampp/htdocs/CharacterBuilder/pages/cards/cards.txt";
-    $file = fopen($f, "r") or die("Unable to open file");
-
-    while(!feof($file))
-    {
-        $type = trim(fgets($file));
-
-        $id = trim(fgets($file));
-        $name = trim(fgets($file));
-        $art = trim(fgets($file));
-        $cost = trim(fgets($file));
-        $element = trim(fgets($file));
-        $rarity = trim(fgets($file));
-
-        $textClauses = array();
-
-        $clause = trim(fgets($file));
-
-        while($clause != "End") {
-            $clauseArray = array();
-            $clauseArray[] = $clause;
-            $textClauses[] = $clauseArray;
-            $clause = trim(fgets($file));
-
-        }
-
-
-        switch($type){
-            case "Action":
-                $subtype = trim(fgets($file));
-                $range = trim(fgets($file));
-
-                $cards[$id] = new Action ($id, $name, $art, $cost, $element, $rarity, $textClauses, $subtype, $range);
-                break;
-            case "Enchantment":
-                $subtype = trim(fgets($file));
-                $range = trim(fgets($file));
-                if(!is_numeric($range) && $range != "null"){
-                    $range = new AreaOfEffect($range, null);
-                }
-                $hp = trim(fgets($file));
-
-                $cards[$id] = new Enchantment($id, $name, $art, $cost, $element, $rarity, $textClauses, $subtype, $range, $hp);
-                break;
-            case "Path":
-
-                $steps = Array();
-
-                $number = trim(fgets($file));
-
-                while($number != "Stop"){
-
-                    $element = trim(fgets($file));
-
-
-                    $subtext = "";
-
-                    $clause = trim(fgets($file));
-
-                    while($clause != "End"){
-
-                        $subtext .= $clause."\n";
-
-                        $clause = trim(fgets($file));
-                    }
-
-
-                    $steps[$number] = new Step($element, $subtext);
-
-                    $number = trim(fgets($file));
-                }
-
-                $cards[$id] = new Path($id, $name, $art, $cost, $element, $rarity, $textClauses, $steps);
-                break;
-            case "Summon":
-                $hp = trim(fgets($file));
-                $movement = trim(fgets($file));
-                $subtype = trim(fgets($file));
-
-                $subtext  = "";
-
-                $clause = trim(fgets($file));
-
-                while($clause != "End"){
-
-                    $subtext .= $clause;
-
-                    $clause = trim(fgets($file));
-                }
-
-                $cards[$id] = new Summon($id, $name, $art, $cost, $element, $rarity, $textClauses, $hp, $movement, new SummonAction($subtype, $subtext));
-                break;
-            default:
-                echo "WTF??";
-
-        }
-
-
-        fgets($file);
+    foreach($array as $element){
+        $numArray[] = $element;
     }
 
-    fclose($file);
-
-    return $cards;
+    return $numArray;
 }
 
-function printStep($step){
+function displayCard($card){
     ?>
-       <p><?php echo $step->getElement().": ".$step->getText()?></p>
+    <img src="cardImages/<?php echo $card->getId()?>.jpg" alt ="<?php echo $card->getName()?>">
+    <br>
+
     <?php
+
 }
 
-function printCard($card){
+   function displayCardRow($cards, $index){?>
+        <?php
+        for($i = $index; $i < $index + 3; $i++){
+            if($i == count($cards)) {
+                return true;
+            }
+            $card = $cards[$i];
+            ?>
+                <img style="display: inline; margin: 0 5px;" src="cardImages/<?php echo $card->getId()?>.jpg" alt ="<?php echo $card->getName()?>">
+            <?php
+        }
+        ?>
 
-    ?>
+<?php
+echo '<br>';
+return false;
+}
 
+?>
     <html lang="en">
+        <head>
+            <link rel="stylesheet" href="/css/bootstrap.css">
+            <link rel="stylesheet" href="/css/style.css">
 
+            <title>Cards</title>
+        </head>
 
+<?php
+    $index = 0;
+    $cards= Card::loadCardsFromFile();
 
-    <!--Make a Magic: the Gathering Card in CSS by Davide Iaiunese consulted-->
-    <div class="card-container">
-        <div class="card-background">
-            <div class="card-frame">
-                <div class="frame-header">
-                    <h1 class="name"><?php echo $card->getName()?></h1>
-                    <span id="mana-icon"><?php echo $card->getCost()?></span>
-                </div>
+    $cards = createNumericallyIndexedArray($cards);
+    $finished = false;
 
-                <!-- Here goes the art-->
-                <img class="frame-art" src="<?php echo $card->getArt()?>" alt="art">
-
-
-                <div class="frame-type-line">
-                    <h1 class="type">
-                        <?php
-
-                        $type = get_class($card);
-                        echo $type;
-
-                        if($type == "Action" || $type == "Enchantment"){
-                            echo " - ".$card->getSubtype();
-                        }
-                         ?>
-                    </h1>
-                    <!-- potential set icon/rarity here -->
-                </div>
-
-                <div class="frame-text-box">
-                    <p class="card-text-1"><?php if($card->getText() != "null"){echo $card->getText();}?></p>
-
-                    <?php
-
-                    if($type == "Path"){
-                        foreach ($card->getSteps() as $step){
-                            printStep($step);
-                        }
-                    }
-                    else if($card->rangeType() == "Range"){
-                        ?>
-                        <p class="range">Range: <?php echo $card->getRange()?></p>
-                        <?php
-                    }
-                    else if ($card->rangeType() == "AOE"){
-                        ?>
-                        <p class="range">AOE: <?php echo $card->getRange()->getName()?></p>
-                        <?php
-                    }
-
-                    if($type == "Enchantment" || $type == "Summon"){
-                        ?>
-                        <p class="hp">HP: <?php echo $card->getHP()?></p>
-                        <?php
-                    }
-
-                    if($type == "Summon"){
-                        ?>
-                        <p class="movement">Movement: <?php echo $card->getMovement()?></p>
-                        <?php if($card->getAction()->getText() != "null"){echo '<br>'.$card->getAction()->getSubtype().': <br>'.$card->getAction()->getText();}?>
-                        <?php
-                    }
-
-
-
-                    ?>
-
-                    <p class="flavour-text"></p>
-                </div>
-
-                <div class="frame-bottom-into inner-margin">
-                    <div class="fbi-left">
-                        <p><?php echo $card->getId()." ".$card->getRarity()?></p>
-                        <p>BS; EN <!-- paintbrush --> Probably Me lol</p>
-                    </div>
-                    <div class="fbi-center"></div>
-                    <div class="fbi-right">
-                        Me
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-
+    while(!$finished) {
+    $finished = displayCardRow($cards, $index);
+    $index += 3;
+    }
+?>
     </html>
-
-    <?php
-}
-
-$cards = loadCardsFromFile();
-
-/*
-//Adding text to image
-$card = $cards["A23"];
-header("Content-type: image/jpeg");
-$imgPath = 'noRange.jpg';
-$image = imagecreatefromjpeg($imgPath);
-$color = imagecolorallocate($image, 0, 0, 0);
-$string = $card->getText()[0];
-$fontSize = 26;
-//start x = 36, end x = 230
-//start y = 260, end y = 340
-$x = 36;
-$y = 340;
-imagestring($image, $fontSize, $x, $y, $string, $color);
-imagejpeg($image, './TestPic.jpg');*/
-
-/*
-
-$card = $cards["A23"];
-
-$actionTemplate = new ActionTemplate($card);
-
-$actionTemplate->printFormattedText();
-
-*/
-
-//$actionTemplate->createTextBox();
-/*
-foreach($cards as $card){
-    printCard($card);
-}*/
 
